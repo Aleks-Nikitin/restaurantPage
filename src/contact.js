56b15a85723d3900ef2bde810789fee035502ee2
Power366 <172065612+Power366@users.noreply.github.com>
2025-07-19 23:36:49 +0700

    all 3 pages work somewhat

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,26 @@
-export function contact(){
+export function contact(cont){
+
+        const titleMenu = document.createElement("div");
+        titleMenu.setAttribute("class","titleMenu");
+        const menuTxt = document.createElement("div");
+        menuTxt.setAttribute("class", "center-text")
+        menuTxt.textContent="Contacts"
+        const menuContent = document.createElement("div");
+        menuContent.setAttribute("class", 'block');
+        const headling = document.createElement("div");
+        headling.setAttribute("class", "headling");
+        headling.textContent= "Personal information";
+        const menuPositions = document.createElement("div");
+        menuPositions.setAttribute("class", "menuPositions");
+        menuPositions.textContent = " Contact List:\n\nEmily Carter - emily.carter@gmail.com - (555) 497-8479\nJames Nguyen - james.nguyen@gmail.com - (555) 987-5944\nSofia Patel - sofia.patel@gmail.com - (555) 645-7789\nLiam Robinson - liam.robinson@gmail.com - (555) 876-9944\nIsabella Kim - isabella.kim@gmail.com - (555) 496-4484\nNoah Thompson - noah.thompson@gmail.com - (555) 678-9964\nAva Martinez - ava.martinez@gmail.com - (555) 475-9945\nEthan Brooks - ethan.brooks@gmail.com - (555) 494-8844";
+            function appending(){
+            cont.appendChild(titleMenu);
+            cont.appendChild(menuContent);
+            menuContent.appendChild(headling)
+            menuContent.appendChild(menuPositions)
+            titleMenu.appendChild(menuTxt);
+            
     
-}
+        }
+        appending()
+    }
--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,31 @@
 import "./menu.css"
+import menuPic from "./fast-food.png";
 export function menu(cont){
-    const title = document.createElement("div");
-    title.setAttribute("class","title");
+    const titleMenu = document.createElement("div");
+    titleMenu.setAttribute("class","titleMenu");
+    const pic = document.createElement("img");
+    pic.setAttribute("id", "menuPic");
+    pic.src = menuPic;
+    const menuTxt = document.createElement("div");
+    menuTxt.setAttribute("class", "center-text")
+    menuTxt.textContent="Menu"
+    const menuContent = document.createElement("div");
+    menuContent.setAttribute("class", 'block');
+    const headling = document.createElement("div");
+    headling.setAttribute("class", "headling");
+    headling.textContent= "Burger Menu:";
+    const menuPositions = document.createElement("div");
+    menuPositions.setAttribute("class", "menuPositions");
+    menuPositions.textContent = "Burger Menu: \n Classic Burger - $6.99\nCheeseburger - $7.49\nBacon Cheeseburger - $8.49\nDouble Patty Burger - $9.99\nVeggie Burger - $ 7.99\nChicken Burger - $7.79\n\nFries - $8.99\nOnion Rings - $6.49\nSoft Drink - $6.99\nMilkshake - $4.99";
+        function appending(){
+        cont.appendChild(titleMenu);
+        cont.appendChild(menuContent);
+        menuContent.appendChild(headling)
+        menuContent.appendChild(menuPositions)
+        titleMenu.appendChild(pic);
+        titleMenu.appendChild(menuTxt);
+        
 
-
+    }
+    appending()
 }
